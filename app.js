4749e69fd1f3c271981b0476b5aaf9e33f84e6d5
BautistaQuetglas <89487143+BautistaQuetglas@users.noreply.github.com>
2022-02-08 05:28:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-app.use(express.static('public'))
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+const users = [];
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.urlencoded());
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Levantando un servidor con Express en ", "http://localhost:3000");
 })
-app.get('/register', (req, res) =>{
-    res.sendFile(path.join(__dirname,'/views/register.html'))
-})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
 app.post('/registro', (req, res) => {
-    res.redirect('/');
-});
-app.get('/login', (req, res) =>{
-    res.sendFile(path.join(__dirname,'/views/login.html'))
-})
-app.post('/ingreso', (req, res) => {
+    console.log(req.body);
+    users.push(req.body);
     res.redirect('/');
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Servidor corriendo en el puerto 3000')
-})
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
